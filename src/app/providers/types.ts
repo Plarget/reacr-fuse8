c679a9edaf7801f67eecd2633ee8bff9215f0bfc
Plarget <67307148+Plarget@users.noreply.github.com>
2023-08-29 14:22:11 +0400

    initial

--- src/app/providers/types.ts
+++ src/app/providers/types.ts
@@ -1,4 +1,4 @@
-import { ReactNode } from "react"
+import type { ReactNode } from "react"
 
 export type TComponentFn = () => ReactNode
 export type TWithProviders = (a: TComponentFn) => () => ReactNode
